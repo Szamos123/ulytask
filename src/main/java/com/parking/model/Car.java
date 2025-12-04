e20114c4a679d42f6f32f9ab1fb67911aaa8633c
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-12-04 21:16:45 +0100

    feat: Car model

--- src/main/java/com/parking/model/Car.java
+++ src/main/java/com/parking/model/Car.java
@@ -1,4 +1,61 @@
 package com.parking.model;
 
-public class Car {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Car implements Serializable {
+
+
+
+    private String licensePlate;
+    private String brand;
+    private String type;
+    private String color;
+
+    private boolean isParked;
+    private LocalDateTime parkingStartDate;
+    private LocalDateTime parkingEndDate;
+
+    public Car(String licensePlate, String brand, String type, String color, boolean isParked) {
+        this.licensePlate = licensePlate;
+        this.brand = brand;
+        this.type = type;
+        this.color = color;
+        this.isParked = false;
+    }
+    public String getLicensePlate() {return licensePlate;}
+    public void setLicensePlate(String licensePlate) {this.licensePlate = licensePlate;}
+
+    public String getBrand() {return brand;}
+    public void setBrand(String brand) {this.brand = brand;}
+
+    public String getType() {return type;}
+    public void setType(String type) {this.type = type;}
+
+    public String getColor() {return color;}
+    public void setColor(String color) {this.color = color;}
+
+    public boolean isParked() {return isParked;}
+    public void setParked(boolean parked) {isParked = parked;}
+
+    public LocalDateTime getParkingEndDate() {return parkingEndDate;}
+    public void setParkingEndDate(LocalDateTime parkingEndDate) {this.parkingEndDate = parkingEndDate;}
+
+    public LocalDateTime getParkingStartDate() {return parkingStartDate;}
+    public void setParkingStartDate(LocalDateTime parkingStartDate) {this.parkingStartDate = parkingStartDate;}
+
+    @Override
+    public int hashCode(){
+        return licensePlate.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(licensePlate, car.licensePlate);
+    }
+
 }
