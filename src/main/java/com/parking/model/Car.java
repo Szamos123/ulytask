9c63032dc104970cbea9b880227f46f9f4c7173f
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-12-06 17:24:55 +0100

    feat: Reset parking method added

--- src/main/java/com/parking/model/Car.java
+++ src/main/java/com/parking/model/Car.java
@@ -6,22 +6,25 @@ import java.util.Objects;
 
 public class Car implements Serializable {
 
-
-
     private String licensePlate;
     private String brand;
     private String type;
     private String color;
-
     private boolean isParked;
+
+
+    private String parkingLotName;
     private LocalDateTime parkingStartDate;
     private LocalDateTime parkingEndDate;
+
+    public Car(){}
 
     public Car(String licensePlate, String brand, String type, String color, boolean isParked) {
         this.licensePlate = licensePlate;
         this.brand = brand;
         this.type = type;
         this.color = color;
+
         this.isParked = false;
     }
     public String getLicensePlate() {return licensePlate;}
@@ -39,11 +42,29 @@ public class Car implements Serializable {
     public boolean isParked() {return isParked;}
     public void setParked(boolean parked) {isParked = parked;}
 
+    public String getParkingLotName() {return parkingLotName;}
+    public void setParkingLotName(String parkingLotName) {this.parkingLotName = parkingLotName;}
+
     public LocalDateTime getParkingEndDate() {return parkingEndDate;}
     public void setParkingEndDate(LocalDateTime parkingEndDate) {this.parkingEndDate = parkingEndDate;}
 
     public LocalDateTime getParkingStartDate() {return parkingStartDate;}
     public void setParkingStartDate(LocalDateTime parkingStartDate) {this.parkingStartDate = parkingStartDate;}
+
+    public boolean isParkingActive(){
+        return isParked && LocalDateTime.now().isBefore(parkingEndDate);
+    }
+
+    public void resetParkingState(){
+        if(isParked && parkingEndDate != null){
+            if(LocalDateTime.now().isAfter(parkingEndDate)){
+                this.isParked=false;
+                this.parkingLotName = null;
+                this.parkingStartDate = null;
+                this.parkingEndDate = null;
+            }
+        }
+    }
 
     @Override
     public int hashCode(){
